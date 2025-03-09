d86fe5f07170362e677402ac8a69e36b8c3f23c8
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2025-03-10 01:25:18 +0500

    models.py, admin.py and locations views are created

---
+++ locations/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import LocationListCreateView, LocationDetailView
+
+urlpatterns = [
+    path('locations/', LocationListCreateView.as_view(), name='location-list-create'),
+    path('locations/<int:pk>/', LocationDetailView.as_view(), name='location-detail'),
+]
---
+++ locations/views.py
@@ -0,0 +1,48 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Location
+from .serializers import LocationSerializer
+from django.http import Http404
+
+
+
+class LocationListCreateView(APIView):
+    def get(self, request):
+        locations = Location.objects.all()
+        serializer = LocationSerializer(locations, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LocationDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return Location.objects.get(pk=pk)
+        except Location.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        location = self.get_object(pk)
+        serializer = LocationSerializer(location)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        location = self.get_object(pk)
+        serializer = LocationSerializer(location, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        location = self.get_object(pk)
+        location.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
